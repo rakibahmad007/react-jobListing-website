d45a3f1a19f627b64ffb1ed2586d9e03524ee7eb
rakibahmad007 <118014776+rakibahmad007@users.noreply.github.com>
2025-01-01 01:39:44 +0600

    Components Breakdown

---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Card = ({children, bg = "bg-blue-200"}) => {
+  return (
+    <div className= {` ${bg} p-6 rounded-lg shadow-md`}>{children}</div>
+  )
+}
+
+export default Card
