9f55be94ac13107b725f3e3338db1fe3ab7a3442
rakibahmad007 <118014776+rakibahmad007@users.noreply.github.com>
2025-01-02 02:15:55 +0600

    Latest Update
    created a NotFoundPage and a json server

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import{ Route, createBrowserRouter, createRoutesFromElements, RouterProvider} fr
 import MainLayout from './layouts/MainLayout'
 import HomePage from './pages/HomePage'
 import JobsPage from './pages/JobsPage'
+import NotFoundPage from './pages/NotFoundPage'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -11,6 +12,8 @@ const router = createBrowserRouter(
 
       <Route index element={<HomePage/>}/>
       <Route path='/jobs' element={<JobsPage/>}/>
+      <Route path='*' element={<NotFoundPage/>}/>
+      {/*  The path attribute is set to '*', which is a wildcard character. This means that this route will match any URL path that does not match any of the other defined routes in the application. Essentially, it acts as a catch-all route for undefined paths. */}
 
    </Route>
     
